8a978631a1310652ff6f3dbfaf388c22ebaa7472
nha <nha@users.noreply.github.com>
2019-11-07 20:31:50 +0000

    bump

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/nha/citrus"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.439" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"]
                  [manifold "0.1.8"]
                  [rum "0.11.4"]]
 
